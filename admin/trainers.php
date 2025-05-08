c0b8fec815d26409d0b07813cae1c43f187d41ca
Muhammad <muharkan04@gmail.com>
2025-05-08 08:53:23 +0700

    Merapihkan Admin Panel

--- admin/trainers.php
+++ admin/trainers.php
@@ -29,14 +29,6 @@
                         <div class="flex items-center">
                             <h1 class="text-xl font-semibold text-gray-800">Trainers</h1>
                         </div>
-                        <div class="flex items-center space-x-4">
-                            <button class="text-gray-500 hover:text-gray-700">
-                                <i class="fas fa-bell"></i>
-                            </button>
-                            <button class="text-gray-500 hover:text-gray-700">
-                                <i class="fas fa-question-circle"></i>
-                            </button>
-                        </div>
                     </div>
                 </div>
             </header>
